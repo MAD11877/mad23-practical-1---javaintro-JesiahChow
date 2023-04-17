5cd3eb1a73476000ca93993ceb4fddb8d2c034a8
JesiahChow <S10221066@connect.np.edu.sg>
2023-04-17 19:07:37 +0800

    testing

--- Question4.java
+++ Question4.java
@@ -1,23 +1,29 @@
 import java.util.Scanner;
 
-public class Question4
-{
-  public static void main(String[] args)
-  {
+public class Question4 {
+  public static void main(String[] args) {
     /**
-     * Prints a right angle triangle with *. The base of the triangle depends on the integer given.
-     *    e.g.
-     *     > 5
-     *     *****
-     *     ****
-     *     ***
-     *     **
-     *     *
+     * Prints a right angle triangle with *. The base of the triangle depends on the
+     * integer given.
+     * e.g.
+     * > 5
+     * *****
+     * ****
+     * ***
+     * **
+     * *
      * 
      * Hint: 2 loops are required. System.out.print(...) to print on single line.
      */
-     
+
     Scanner in = new Scanner(System.in);
-    
+    System.out.println("Enter a whole number you wish to give for the base of triangle: ");
+    int height = in.nextInt();
+    for (int i = 1; i < height; i++) {
+      for (int j = 0; j < height - i; j++) {
+        System.out.print("*" + " ");
+      }
+      System.out.println();
+    }
   }
 }
